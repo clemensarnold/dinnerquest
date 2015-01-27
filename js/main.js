3410c29e08263c82a15932ef1bf3ca3b890648ff
Clemens Arnold <clemens@havefunwashing.net>
2015-01-27 12:17:11 +0100

    resized img

--- js/main.js
+++ js/main.js
@@ -865,6 +865,8 @@ var dq = (function($, window, undefined) {
 
             scenarioNr = helper.getRandomNumber(app.json.scenarios.length);
 
+            scenarioNr = 4;
+            scenarioMood = 'positive';
 
             log('scenarioNr: ' + scenarioNr);
             
